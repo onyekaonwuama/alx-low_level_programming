a854d6fd108e77472d7475b6fefb1feb3bcb827a
Nnyeka <nnyeka212@gmail.com>
2022-09-08 22:37:00 -0700

    task-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - Entry point
-
- *
-
  * Description: prints "Programming is like building a multilingual puzzle"
-
- *
-
- * Return: Always 0 (Success)
-
+ * Return: Always 0
  */
-
 int main(void)
-  
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
