bf2dc920c19f4e66131c4b31962f9d6f5f09f9e5
Nnyeka <nnyeka212@gmail.com>
2022-09-13 10:38:15 -0700

    task-2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - determine if number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
+i = 97;
+j = 65;
+while (i < 123)
+{
+putchar(i);
+i++;
+}
+while (j < 91)
+{
+putchar(j);
+j++;
+}
+putchar(10);
+return (0);
+}
