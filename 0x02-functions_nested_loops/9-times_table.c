a1b06d2ac57294aa5ec7ef695d06bce029fb5fff
Nnyeka <Nnyeka212@gmail.com>
2022-09-16 08:20:59 -0700

    task-9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,14 @@ void times_table(void)
 int row, col, mul, tens, ones;
 for (row = 0; row <= 9; row++)
 {
-for (col = 0; column < 10; col++)
+for (col = 0; col < 10; col++)
 {
 mul = row * col;
 tens = mul / 10;
 ones = mul % 10;
 if (col == 0)
 {
-putchar('0');
+_putchar('0');
 }
 else if (mul < 10)
 {
@@ -26,11 +26,11 @@ _putchar(' ');
 _putchar(ones + '0');
 }
 else
+{
 _putchar(',');
 _putchar(' ');
 _putchar(tens + '0');
 _putchar(ones + '0');
-}
 }
 _putchar('\n');
 }
