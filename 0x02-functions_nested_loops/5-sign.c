8d546faa4072a6fb88e6bd20eed48c8875d9b51f
Nnyeka <Nnyeka212@gmail.com>
2022-09-16 04:32:18 -0700

    task-5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ * @n: n is an int
+ * Return: 1 if +ve, -1 if -ve else 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43)
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(45)
+		return (0);
+	}
+	else
+	{
+		_putchar(48)
+		return (-1)
+	}
+}
+
