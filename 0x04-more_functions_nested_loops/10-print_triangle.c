307c055aae226c928706ded82bb9ca5da74ada6c
Nnyeka <Nnyeka212@gmail.com>
2022-09-16 22:24:42 -0700

    task-10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - outputs triangle with #
+ * @size: is an int
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int row, gap;
+
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (row = 0; row < size; row++)
+		{
+			for  (gap = (size - 1); gap >= 0; gap--)
+			{
+				if (gap <= row)
+					_putchar('#')
+				else
+					_putchar('')
+			}
+		}
+		_putchar('\n')
+	}
+}
+
