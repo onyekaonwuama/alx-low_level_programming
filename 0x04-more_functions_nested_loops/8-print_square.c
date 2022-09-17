04c8ac2d280a1383803a200e5d7bf6b5000879b4
Nnyeka <Nnyeka212@gmail.com>
2022-09-16 22:03:43 -0700

    task-8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - entry point
+ * @size: is an int
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (col = 0; col < size; col++)
+		{
+			for (row = 0; row < size; row++)
+			{
+				_putchar('\n');
+			}
+			_putchar('\n');
+		}
+	}
+}
