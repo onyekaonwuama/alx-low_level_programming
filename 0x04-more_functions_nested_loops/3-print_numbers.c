bde23f17449dd4b1472e4efc4ce3fdec1a5471a0
Nnyeka <Nnyeka212@gmail.com>
2022-09-16 17:04:46 -0700

    task-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c < '10'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
