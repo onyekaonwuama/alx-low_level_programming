2ea00536a9a7201fcbf51193810aaea52144b29b
Nnyeka <Nnyeka212@gmail.com>
2022-09-17 02:38:07 -0700

    task-11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int prime = 612852475143;
+	long int ans = prime;
+	long int div = 2;
+
+	while (ans = div)
+	{
+		if (ans % div == 0)
+		{
+			ans = ans / div;
+		}
+		else
+		{
+			div++;
+		}
+	}
+	printf("%li\n", ans);
+
+	return (0);
+}
+
