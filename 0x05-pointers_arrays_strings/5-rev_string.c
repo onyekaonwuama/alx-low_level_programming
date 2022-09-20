384d5b8d444d19100514f63e34d9275c855faa71
Nnyeka <Nnyeka212@gmail.com>
2022-09-19 22:28:26 -0700

    task-5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses a string
+ * @s: char
+ */
+void print_rev(char *s)
+{
+	int a, len;
+
+	len = strlen(s)
+	for (a = (len - 1); i >= 0; i--)
+		_putchar(s[a]);
+	putchar('\n');
+}
+
