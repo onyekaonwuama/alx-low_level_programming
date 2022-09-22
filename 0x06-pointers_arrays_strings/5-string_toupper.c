699e9df016c1af4a95fcfa8c254cdcd761e9f032
Nnyeka <Nnyeka212@gmail.com>
2022-09-21 18:43:48 -0700

    task-5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase chars to uppercase
+ * @p: The string will be modified
+ * Return: char
+ */
+
+char *string_toupper(char *p)
+{
+	int i = 0;
+
+	while (p[i])
+	{
+		if (p[i] >= 97 && p[i] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		i++;
+	}
+	return (p);
+}
+
