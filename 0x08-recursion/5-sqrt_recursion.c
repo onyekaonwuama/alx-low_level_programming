f9a3414459361f2c933cd1bf100c0508a312b824
Nnyeka <Nnyeka212@gmail.com>
2022-09-29 08:50:18 -0700

    task-5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - square root
+ *@n: int
+ *Return: sqaured val
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt2(1, n));
+}
+/**
+ *_sqrt2 - find square
+ *@a: int
+ *@b: int
+ *Return: sqaured val
+ */
+int _sqrt2(int a, int b)
+{
+	if (a * a > b)
+		return (-1);
+	else if (a * a == b)
+		return (a);
+	else
+		return (_sqrt2(a += 1, b));
+}
+
