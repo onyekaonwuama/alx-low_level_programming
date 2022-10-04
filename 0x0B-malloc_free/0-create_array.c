a97eeb0f990916292c2edabd2ca4149b1a6f8ca0
Nnyeka <Nnyeka212@gmail.com>
2022-10-04 12:44:52 -0700

    task-0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Create an array of characters.
+ * @c: character to replace
+ * @size: the size of the array to craete
+ *
+ * Return: ptr to array or null
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *ptr;
+
+	if (size <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(char) * size);
+
+	if (p != NULL)
+	{
+		for (i = 0; i < size; i++)
+			*(ptr + 1) = c;
+	}
+	else
+	{
+		return NULL
+	}
+	return (ptr);
+}
+
