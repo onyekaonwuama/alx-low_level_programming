2d0f205e3c0bca9de4c497febb613550a01a196b
Nnyeka <Nnyeka212@gmail.com>
2022-10-06 19:52:16 -0700

    task-2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,52 @@
 #include "main.h"
-#include<stdio.h>
-#include<stdlib.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * *string_nconcat - function concatenates two strings
+ *string_nconcat - returns a function that concats two strs
  *
- *@s1: char pointer
- *@s2: char pointer
- *@n: unsigned int
- *
- * Return: pointer or NULL
+ *@s1:str1
+ *@s2:str2
+ *@n:length of str two
+ *Return:pointer
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int str1, str2, i, x;
+	unsigned int i, slen1, slen2, length, j;
 	char *ptr;
 
-	if (s1 = NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	if (s2 = NULL)
+	if (s2 == NULL)
+	{
 		s2 = "";
-	for (str1 = 0; s1[i] != '\0'; str1++)
-		;
-	for (str2 = 0; s2[i] != '\0'; str2++)
+	}
+	slen1 = 0;
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		slen1++;
+	slen2 = 0;
+	for (i = 0; *(s2 + i) != '\0'; i++)
+		slen2++;
+	if (n >= slen2)
+	{
+		n = slen2;
+	}
+	length = slen1 + n;
+	ptr = malloc((sizeof(char) * length) + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < slen1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; i < length; j++, i++)
+	{
+		ptr[i] = s2[j];
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
