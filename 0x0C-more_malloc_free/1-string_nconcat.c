7a4dd599823285ebbbc19b41f1b500df85554227
Nnyeka <Nnyeka212@gmail.com>
2022-10-06 17:48:45 -0700

    task-3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,14 +18,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 == "";
+		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
-	for (str1 = 0; s1[str1] != '\0'; str1++)
+		s2 = "";
+	for (str1 = 0; s1[i] != '\0'; str1++)
 		;
-	for (str2 = 0; s2[str2] != '\0'; str2++)
+	for (str2 = 0; s2[i] != '\0'; str2++)
 		;
-	if (n <= str2)
+	if (n >= str2)
 		n = str2;
 	ptr = malloc(sizeof(char) * (str1 + n + 1));
 
@@ -35,11 +35,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i = 0; i < str1; i++)
 	{
-		ptr[i] = str1[i];
+		ptr[i] = s1[i];
 	}
 	for (j = 0; j < n; j++)
 	{
-		ptr[str1 + j] = str2[j];
+		ptr[str1 + j] = s2[j];
 	}
 	ptr[str1 + j] = '\0';
 }
