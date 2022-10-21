9f80cc411581bf0eabd0aa089a31e114e51d905a
Nnyeka <Nnyeka212@gmail.com>
2022-10-21 14:17:31 -0700

    zask-0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,19 +7,11 @@
 
 
 /**
-
  * struct list_s - singly linked list
-
  * @str: string - (malloc'ed string)
-
  * @len: length of the string
-
  * @next: points to the next node
-
- *
-
  * Description: singly linked list node structure
-
  */
 
 
