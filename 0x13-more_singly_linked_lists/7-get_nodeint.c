a6e55bc70f54af0813e939f8994e91ec32b780f1
Nnyeka <Nnyeka212@gmail.com>
2022-10-25 18:08:47 -0700

    task-7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	for (x = 0; x < index; y++)
+	for (x = 0; x < index; x++)
 	{
 		head = head->next;
 		if (head == NULL)
