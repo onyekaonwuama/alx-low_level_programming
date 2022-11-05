2605ff73528a4ead2d7fec10f51ab365a1247390
Nnyeka <Nnyeka212@gmail.com>
2022-11-04 22:59:10 -0700

    task-3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @ac: number of arguments
+ * @av: argument vector
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+int main(int ac, char **av)
+{
+	int file_from, file_to, read_from;
+	char buf[1024];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
+
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while ((read_from = read(file_from, buf, 1024)) > 0)
+	{
+		if (file_to < 0 || (write(file_to, buf, read_from) != read_from))
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
+
+	if (read_from == -1)
+		dprintf(STDERR_FILENO, "Error; Can't read from %s\n", av[1]), exit(98);
+
+	if ((close(file_from)) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+
+	if ((close(file_to)) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+
+	return (0);
+}
